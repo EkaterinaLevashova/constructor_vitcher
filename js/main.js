60f2880969375e355e3fbbc0dafa3d7587d27a5a
Ekaterina Levashova <katrin.levashova@gmail.com>
2021-07-20 19:31:06 +0300

    first init

---
+++ js/main.js
@@ -0,0 +1,51 @@
+/* 
+new Swiper('.swiper-container', {
+	loop: true,
+	navigation: {
+		nextEl: '.arrow',
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 20
+		},
+		541: {
+			slidesPerView: 2,
+			spaceBetween: 40
+		}
+	}
+});
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.header');
+menuButton.addEventListener('click', function () {
+	menuButton.classList.toggle('menu-button-active');
+	menu.classList.toggle('header-active');
+})
+*/
+const getElement = (tagName) =>{
+	const element = document.createElement(tagName);
+
+	return element;
+}
+
+const createHeader = (param) => {
+	const header = getElement('header');
+	return header;
+};
+
+const movieConstructor = (selector, options) => {
+
+	const app = document.querySelector(selector);
+	
+	if (options.header) {
+		app.append(createHeader(options.header));
+	}
+};
+
+movieConstructor('.app',  {
+	title: 'Ведьмак',
+	header: {
+		logo: 'img src="witcher/logo.png" alt="логотип Ведьмак" class="logo"'
+	}
+});
